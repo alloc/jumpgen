4df42260a370be4066c56c752e6d5a12ecdaed9b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-10-16 14:45:35 -0400

    feat: add `cause` option to `watch` method

--- packages/jumpgen/src/util/matcher-array.ts
+++ packages/jumpgen/src/util/matcher-array.ts
@@ -1,6 +1,7 @@
 import { FSWatcher } from 'chokidar'
 import path from 'node:path'
 import picomatch from 'picomatch'
+import { castArray } from 'radashi'
 
 export type Matcher = {
   base: string
@@ -10,8 +11,9 @@ export type Matcher = {
 }
 
 export class MatcherArray {
-  private files = new Set<string>()
-  private matchers: Matcher[] = []
+  #watchedFiles = new Set<string>()
+  #blamedFiles = new Map<string, Set<string>>()
+  #matchers: Matcher[] = []
 
   /**
    * When defined, base directories from `this.add` calls and files from
@@ -21,10 +23,15 @@ export class MatcherArray {
 
   /**
    * A set of files that have been explicitly watched. Base directories
-   * from `this.add` calls are not included.
+   * from `this.add` calls are not included. Files added with a `cause`
+   * option are not included.
    */
   get watchedFiles(): ReadonlySet<string> {
-    return this.files
+    return this.#watchedFiles
+  }
+
+  get blamedFiles(): ReadonlyMap<string, ReadonlySet<string>> {
+    return this.#blamedFiles
   }
 
   add(patterns: string | string[], options?: picomatch.PicomatchOptions): void {
@@ -52,13 +59,13 @@ export class MatcherArray {
       const { base, glob } = picomatch.scan(pattern)
       const depth = path.normalize(base).split(path.sep).length
 
-      let index = this.matchers.findIndex(m => depth > m.depth)
+      let index = this.#matchers.findIndex(m => depth > m.depth)
       if (index === -1) {
-        index = this.matchers.length
+        index = this.#matchers.length
       }
 
       this.watcher?.add(base)
-      this.matchers.splice(index, 0, {
+      this.#matchers.splice(index, 0, {
         base,
         glob,
         depth,
@@ -67,16 +74,27 @@ export class MatcherArray {
     }
   }
 
-  addFile(file: string): void {
+  addFile(file: string, options?: { cause?: string | string[] }): void {
     this.watcher?.add(file)
-    this.files.add(file)
+    this.#watchedFiles.add(file)
+
+    if (options?.cause) {
+      let blamedFiles = this.#blamedFiles.get(file)
+      if (!blamedFiles) {
+        blamedFiles = new Set()
+        this.#blamedFiles.set(file, blamedFiles)
+      }
+      for (const cause of castArray(options.cause)) {
+        blamedFiles.add(cause)
+      }
+    }
   }
 
   match(file: string): boolean {
-    if (this.files.has(file)) {
+    if (this.#watchedFiles.has(file)) {
       return true
     }
-    for (const matcher of this.matchers) {
+    for (const matcher of this.#matchers) {
       if (matcher.match(file)) {
         return true
       }
@@ -86,14 +104,15 @@ export class MatcherArray {
 
   clear(): void {
     if (this.watcher) {
-      for (const file of this.files) {
+      for (const file of this.#watchedFiles) {
         this.watcher.unwatch(file)
       }
-      for (const matcher of this.matchers) {
+      for (const matcher of this.#matchers) {
         this.watcher.unwatch(matcher.base)
       }
     }
-    this.files.clear()
-    this.matchers.length = 0
+    this.#watchedFiles.clear()
+    this.#blamedFiles.clear()
+    this.#matchers.length = 0
   }
 }
