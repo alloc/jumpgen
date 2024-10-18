5c934020deaab506cd549be69b8210a54345841c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-10-17 22:15:27 -0400

    chore: fix cjs issue with fdir override

--- packages/jumpgen/tsup.config.ts
+++ packages/jumpgen/tsup.config.ts
@@ -1,7 +1,13 @@
+import alias from 'esbuild-plugin-alias'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/generator.ts', 'src/context.ts'],
   format: ['esm'],
   dts: true,
+  esbuildPlugins: [
+    alias({
+      fdir: new URL('../fdir/src/index.ts', import.meta.url).pathname,
+    }),
+  ],
 })
