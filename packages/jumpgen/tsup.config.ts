30ff8a03f8d0599233ee0855b824e57e36f7741c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-10-17 22:15:44 -0400

    chore: use treeshake=smallest in tsup config

--- packages/jumpgen/tsup.config.ts
+++ packages/jumpgen/tsup.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   entry: ['src/generator.ts', 'src/context.ts'],
   format: ['esm'],
   dts: true,
+  treeshake: 'smallest',
   esbuildPlugins: [
     alias({
       fdir: new URL('../fdir/src/index.ts', import.meta.url).pathname,
